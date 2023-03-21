4b28e256a1164ff6edbba490cfec04a55b90f321
Peter Hahn Boelt <peterhahnboelt@gmail.com>
2023-03-21 12:02:21 +0100

    SWAPI: Caching implemented

--- RegionNordCode/SwapiAPI/Program.cs
+++ RegionNordCode/SwapiAPI/Program.cs
@@ -1,17 +1,20 @@
-﻿
-using RestSharp;
+﻿using RestSharp;
 using System.Text.Json;
+using System.Runtime.Caching;
+using System;
 
 namespace SwapiAPI;
 public class Lookup
 {
-    //TODO Cache
-
 
     static void Main(string[] args)
     {
+        //Adding a client for API calls
         RestClient client = new RestClient("https://swapi.dev/api/");
 
+        // Cache 
+        MemoryCache cache = new MemoryCache("SWAPI");
+        
         bool running = true;
         Console.WriteLine("Welcome to the SWAPI API people lookup ");
         Console.WriteLine("Type 'exit' to stop)");
@@ -32,7 +35,7 @@ public class Lookup
             {
                 try
                 {
-                    Console.WriteLine(GetPerson(userInput, client));
+                    Console.WriteLine(GetPerson(userInput, client, cache));
                 }
                 catch (Exception ex)
                 {
@@ -44,7 +47,7 @@ public class Lookup
         }
     }
 
-    private static String GetPerson(string id, RestClient client)
+    public static string GetPerson(string id, RestClient client, MemoryCache cache)
     {
         // Check if the id is a integer
         if (!Int32.TryParse(id, out var personId))
@@ -52,18 +55,26 @@ public class Lookup
             throw new Exception("The provided ID is not a number");
         }
 
+        string json = null;
+
         //TODO: check the cache for the id
+        var item = cache.GetCacheItem(id);
+        if (item != null)
+        {
+            json = item.Value.ToString();
+        }
+        else
+        {
+            //TODO: Maybe make this parameterized to combat SQL injection (Or do we trust SWAPI to handle that? )
+            var request = new RestRequest("people/" + id);
 
-        //TODO: Maybe make this parameterized to combat SQL injection (Or do we trust SWAPI to handle that? )
-        var request = new RestRequest("people/" + id);
+            var response = client.Get(request);
+            json = response.Content;
 
-        var response = client.Get(request);
+            cache.Add(new CacheItem(id, json), new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(120.0) });
+        }
 
-        //Get the Json from the response
-        string json = response.Content;
-
-        //Deserialize into a person object
-        Person? person = JsonSerializer.Deserialize<Person>(json);
+        Person person = JsonSerializer.Deserialize<Person>(json);
 
         return person.ToString();
     }
